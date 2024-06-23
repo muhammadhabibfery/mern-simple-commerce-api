92cbf5955e0947838c3cbf2df32575c2abfa72b3
Muhammad Habib Fery <muhammadhabibfery@gmail.com>
2024-06-23 21:18:00 +0700

    wip: update cookie's config

--- src/config/cookie.js
+++ src/config/cookie.js
@@ -1,13 +1,13 @@
 export const setCookie = {
 	httpOnly: true,
 	expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // one day
-	secure: process.env.NODE_ENV === "dev" ? true : false,
+	secure: process.env.NODE_ENV === "dev" ? false : true,
 	signed: true,
 };
 
 export const clearCookie = {
 	httpOnly: true,
 	expires: new Date(Date.now()),
-	secure: process.env.NODE_ENV === "dev" ? true : false,
+	secure: process.env.NODE_ENV === "dev" ? false : true,
 	signed: true,
 };
