ba3ec52fc0d3751c9d8d68330f8664b07f8a8f81
Muhammad Habib Fery <muhammadhabibfery@gmail.com>
2024-06-24 18:56:34 +0700

    wip: update validate function

--- src/application/validations/validate.js
+++ src/application/validations/validate.js
@@ -1,17 +1,27 @@
 import ValidationError from "../../errors/validationError.js";
 
+const schemaConfig = { abortEarly: false, allowUnknown: true };
+
 const validate = async (schema, body, file = null, async = false) => {
+	const schemaKeys = Object.keys(schema.describe().keys);
 	let data = { ...body };
 
 	if (file) data = { ...data, ...file };
 
 	const result = async
-		? await schema.validateAsync(data, { abortEarly: false })
-		: schema.validate(data, { abortEarly: false });
+		? await schema.validateAsync(data, schemaConfig)
+		: schema.validate(data, schemaConfig);
 
-	if (result?.error) {
-		throw new ValidationError(result.error.message);
-	} else return result?.value || result;
+	if (result?.error) throw new ValidationError(result.error.message);
+	else return result?.value && getValuesFromKeys(schemaKeys, result.value);
+};
+
+const getValuesFromKeys = (keys, result) => {
+	let data = {};
+	keys.reduce((_, key) => {
+		if (result.hasOwnProperty(key)) data[key] = result[key];
+	}, []);
+	return data;
 };
 
 export default validate;
