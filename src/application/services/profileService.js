793578771bbc059922b93ee0151cf36134da6e1f
Muhammad Habib Fery <muhammadhabibfery@gmail.com>
2024-06-27 19:41:15 +0700

    refactor(user): refactor profile service
    refactor logic in update profile and password user

--- src/application/services/profileService.js
+++ src/application/services/profileService.js
@@ -1,37 +1,29 @@
 import NotFoundError from "../../errors/notFoundError.js";
-import { checkItem, checkPassword, setParams } from "../../utils/global.js";
+import { checkPassword, modelAction } from "../../utils/global.js";
 import User from "../models/userModel.js";
-import {
-	changePasswordValidation,
-	updateProfileValidation,
-} from "../validations/profileValidation.js";
+import { changePasswordValidation, updateProfileValidation } from "../validations/profileValidation.js";
 import validate from "../validations/validate.js";
 import ValidationError from "../../errors/validationError.js";
 
 const updateProfile = async ({ body, user }) => {
 	const data = await validate(updateProfileValidation, body);
-	const params = setParams(
-		"update",
-		User,
-		{ _id: user.id },
-		new NotFoundError("User not found"),
-		data
-	);
-
-	await checkItem(params);
+	await modelAction({
+		model: User,
+		action: "update",
+		queries: { _id: user.id },
+		errClass: new NotFoundError("User not found"),
+		data,
+	});
 };
 
 const updatePassword = async ({ body, user }) => {
-	const { current_password, new_password } = await validate(
-		changePasswordValidation,
-		body
-	);
-	user = await User.findById({ _id: user.id });
-	await checkPassword(
-		user,
-		current_password,
-		new ValidationError(`"current_password" invalid`)
-	);
+	const { current_password, new_password } = await validate(changePasswordValidation, body);
+	user = await modelAction({
+		model: User,
+		action: "get",
+		queries: { _id: user.id },
+	});
+	await checkPassword(user, current_password, new ValidationError(`"current_password" invalid`));
 
 	user.password = new_password;
 	user.save();
