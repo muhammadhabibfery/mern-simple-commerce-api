9a3f2033ed690d5797098918c2f348570d15855d
Muhammad Habib Fery <muhammadhabibfery@gmail.com>
2024-06-27 19:34:33 +0700

    refactor(user): refactor user service
    refactor logic in update user for admin

--- src/application/services/userService.js
+++ src/application/services/userService.js
@@ -1,5 +1,5 @@
 import NotFoundError from "../../errors/notFoundError.js";
-import { checkItem, setParams, wrapData } from "../../utils/global.js";
+import { modelAction, wrapData } from "../../utils/global.js";
 import User from "../models/userModel.js";
 import { updateUser } from "../validations/userValidation.js";
 import validate from "../validations/validate.js";
@@ -9,18 +9,10 @@ const getAll = async ({ query, user }) => {
 	let queryObject = { _id: { $ne: user.id } };
 	const sortList = "-createdAt -_id";
 
-	// if (search) queryObject.name = { $regex: search, $options: "i" };
-	if (search)
-		queryObject.$or = [
-			{ name: { $regex: search, $options: "i" } },
-			{ email: { $regex: search, $options: "i" } },
-		];
+	if (search) queryObject.$or = [{ name: { $regex: search, $options: "i" } }, { email: { $regex: search, $options: "i" } }];
 	if (role) queryObject.role = role;
 
-	const users = await User.find(queryObject)
-		.sort(sortList)
-		.skip(skip)
-		.limit(size);
+	const users = await User.find(queryObject).sort(sortList).skip(skip).limit(size);
 	const totalUsers = await User.countDocuments(queryObject);
 
 	return wrapData(users, totalUsers, { page, size });
@@ -29,15 +21,14 @@ const getAll = async ({ query, user }) => {
 const update = async ({ params, body, user }) => {
 	let data = await validate(updateUser, body);
 	data = { ...data, updatedBy: user.id };
-	const payload = setParams(
-		"update",
-		User,
-		{ _id: params.id },
-		new NotFoundError("User not found"),
-		data
-	);
 
-	await checkItem(payload);
+	await modelAction({
+		model: User,
+		action: "update",
+		queries: { _id: params.id },
+		errClass: new NotFoundError("User not found"),
+		data,
+	});
 };
 
 export default { getAll, update };
