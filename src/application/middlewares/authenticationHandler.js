42a0164a2461ee70b67bc65f953648b3da5faf59
Muhammad Habib Fery <muhammadhabibfery@gmail.com>
2024-07-05 17:13:47 +0700

    refactor(auth): refactor authentication handler middleware

--- src/application/middlewares/authenticationHandler.js
+++ src/application/middlewares/authenticationHandler.js
@@ -1,18 +1,41 @@
 import jwt from "jsonwebtoken";
 import UnauthenticatedError from "../../errors/unauthenticatedError.js";
+import Token from "../models/tokenModel.js";
+import User from "../models/userModel.js";
 
 const authenticationHandler = async (req, res, next) => {
-	try {
-		let token = req?.signedCookies?.refreshToken;
-		const authHeader = req.headers.authorization;
-		if (authHeader && authHeader.startsWith("Bearer "))
-			token = token.split(" ")[1];
+	const unauthenticatedMessage = "Unauthenticated";
 
-		const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+	try {
+		let user;
+		const { accessToken, refreshToken } = req?.signedCookies;
+		const authHeader = req.headers.authorization;
+
+		if (authHeader && authHeader.startsWith("Bearer ")) {
+			const token = authHeader.split(" ")[1];
+			user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+		}
+		if (accessToken) {
+			user = jwt.verify(accessToken, process.env.JWT_SECRET_KEY);
+		}
+		if (!accessToken && refreshToken) {
+			const { refreshToken: validRefreshToken } = jwt.verify(refreshToken, process.env.JWT_SECRET_KEY);
+			const modelData = { action: "get", errClass: new UnauthenticatedError(unauthenticatedMessage) };
+			await modelAction({
+				...modelData,
+				model: Token,
+				queries: { refreshToken: validRefreshToken, user: user.id },
+			});
+			user = await modelAction({ ...modelData, model: User, queries: { _id: user.id } });
+			attachCookies({ res, user, refreshToken: validRefreshToken });
+		}
+
+		if (!user) throw new UnauthenticatedError(unauthenticatedMessage);
+
 		req.user = user;
 		next();
 	} catch (error) {
-		throw new UnauthenticatedError("Unauthenticated");
+		throw new UnauthenticatedError(unauthenticatedMessage);
 	}
 };
 
