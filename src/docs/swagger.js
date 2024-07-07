efe003a5dd2893f7e2f76075e4aa01caef091dec
Muhammad Habib Fery <muhammadhabibfery@gmail.com>
2024-07-07 14:56:05 +0700

    docs: update api docs

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,7 +1,11 @@
 import swaggerUi from "swagger-ui-express";
 import { baseRoute } from "../config/global.js";
-import apiDocs from "./api-docs.json" with { type: "json" };
+import apiDocs from "./api-docs.json" assert { type: "json" };
+
+const options = {
+	customSiteTitle: "MERN Commerce - API Documentation",
+};
 
 export default (app) => {
-	app.use(`${baseRoute}/api-docs`, swaggerUi.serve, swaggerUi.setup(apiDocs));
+	app.use(`${baseRoute}/api-docs`, swaggerUi.serve, swaggerUi.setup(apiDocs, options));
 };
